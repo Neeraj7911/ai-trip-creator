5487107c9a27cd80a4b53f7a9966c26393559445
Neeraj7911 <ns1399@srmist.edu.in>
2024-08-03 13:05:54 +0530

    add ejoi to button

--- ai-trip-creator/src/create-trip/index.jsx
+++ ai-trip-creator/src/create-trip/index.jsx
@@ -1,27 +1,32 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import React from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import "./index.css";
-//import { Input } from "postcss";
 import { Input } from "../components/ui/input";
-//import Input from "./Input";
 import { SelectBudgetOptions, SelectTravelesList } from "@/constants/options";
 import { Button } from "@/components/ui/button";
 
 function CreateTrip() {
+  const [showEmoji, setShowEmoji] = useState(true);
+
+  const handleClick = () => {
+    setShowEmoji(false); // Hide the emoji when the button is clicked
+  };
+
   return (
     <div className="b1">
-      <div className="sm:px-10 md:px32 lg:px-56 xl:px-10 px-5 mt-10">
+      <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
         <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
         <p className="mt-3 text-gray-500 text-xl">
-          Just provide some basic information, and out trip planner will
+          Just provide some basic information, and our trip planner will
           generate a <br />
           customized itinerary based on your preferences.
         </p>
         <div className="mt-20">
           <div>
             <h2 className="text-xl my-3 font-medium">
-              What is destination of choice?
+              What is the destination of choice?
             </h2>
             <GooglePlacesAutocomplete
               apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
@@ -67,7 +72,7 @@ function CreateTrip() {
           </div>
           <div className="some-class" style={{ width: "999px" }}>
             <h2 className="text-xl my-3 font-medium mt-9">
-              Who do you plan on travelling with on your next adventure?
+              Who do you plan on traveling with on your next adventure?
             </h2>
             <div className="grid grid-cols-3 gap-7 mt-5">
               {SelectTravelesList.map((item, index) => (
@@ -86,7 +91,13 @@ function CreateTrip() {
         </div>
       </div>
       <div>
-        <Button className="my-10 float-right flex mr-96">Generate</Button>
+        <Button
+          className="my-10 float-right flex mr-96 button-with-emoji"
+          onClick={handleClick}
+        >
+          Generate
+          {showEmoji && <span className="emoji">🚀</span>}
+        </Button>
       </div>
     </div>
   );
